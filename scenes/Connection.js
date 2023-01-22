524c8179ca756900569ee433a5b41b12f143e8f1
anison99_ <anita.zych9941@gmail.com>
2023-01-22 15:47:02 +0100

    add bluetooth connection

--- scenes/Connection.js
+++ scenes/Connection.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
 
-import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,} from 'react-native';
-import {Colors,DebugInstructions,Header,LearnMoreLinks,ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
+const manager = new BleManager();
 
+export default function Connection() {
+  const [devices, setDevices] = useState([]);
+  const [scanning, setScanning] = useState(false);
 
-function Connection() {
-  
+  useEffect(() => {
+    const subscription = manager.onStateChange((state) => {
+      if (state === 'PoweredOn') {
+        scanAndConnect();
+        subscription.remove();
+      }
+    }, true);
+    return () => subscription.remove();
+  }, [manager]);
+
+  const scanAndConnect = () => {
+    setScanning(true);
+    manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error)
+        setScanning(false);
+        return;
+      }
+
+      if (!devices.find((d) => d.id === device.id)) {
+        setDevices([...devices, device]);
+      }
+    });
+  };
+
+  const stopScanning = () => {
+    manager.stopDeviceScan();
+    setScanning(false);
+  };
 
   return (
-    <>
-    </>
-    
+    <View>
+      <Text>Devices:</Text>
+      {devices.map((device) => (
+        <Text key={device.id}>{device.name} ({device.id})</Text>
+      ))}
+      {scanning ? (
+        <Button title="Stop scanning" onPress={stopScanning} />
+      ) : (
+        <Button title="Scan for devices" onPress={scanAndConnect} />
+      )}
+    </View>
   );
 }
-
-export default Connection;
-
